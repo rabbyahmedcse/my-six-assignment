d147bc95cf3417764c08f97468127c6e185dc418
rabbyahmedcse <ahmed242-15-828@diu.edu.bd>
2026-09-26 16:13:34 +0600

    Final tuch BackGround color now  fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-screen flex flex-col bg-black text-white">
       <PersonsContextProvider>
       <Navbar></Navbar>
       {children}
